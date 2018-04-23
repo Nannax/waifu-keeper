065f1fa9826e96abb0198d332f3a27310919facf
Nannax <lorenz.nanni@gmail.com>
2018-04-23 10:33:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,6 @@ const config = {
     "prefix": process.env.PREFIX,
     "waifu_master_id": process.env.WAIFU_MASTER_ID,
     "cx_google_id": process.env.CX_GOOGLE_ID,
-    "google_key": process.env.GOOGLE_KEY,
-    "matto_id": process.env.MATTO_ID,
-    "matto_url": process.env.MATTO_URL
+    "google_key": process.env.GOOGLE_KEY
   }
 module.exports = config;
