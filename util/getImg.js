06ab38b8df7bd535fee4bc9224df85e5dc070052
Nannax <lorenz.nanni@gmail.com>
2018-04-26 11:07:22 +0200

    Optimized image searching

--- util/getImg.js
+++ util/getImg.js
@@ -6,5 +6,6 @@ exports.get = async (waifu, client) => {
 
     const response = await snekfetch.get("https://www.googleapis.com/customsearch/v1?key="+client.config.google_key+"&cx="+client.config.cx_google_id+"&start="+index+"&num=1&imgType=face&searchType=image&q="+waifu);
     client.logger.log("Img URL: " + response.body.items[0].link);
+    
     return response.body.items[0].link;
 }
